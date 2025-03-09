23c5d842ae376b03dca96c4384db391a8bc8a941
samir20-23 <germanysamir1@gmail.com>
2025-03-09 02:42:55 +0000

    ver4

--- Bootcamp/mastodonApp/db/migrate/20250224144617_add_effective_date_to_terms_of_services.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddEffectiveDateToTermsOfServices < ActiveRecord::Migration[8.0]
-  def change
-    add_column :terms_of_services, :effective_date, :date
-  end
-end
