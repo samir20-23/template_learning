91aa2294ed2eae92a6eb37d6ee8591f1d2495b5e
samir20-23 <aouladamarsamir@gmail.com>
2025-07-18 20:20:42 +0100

    fix(UI):right pages imgs , blur filter

--- Templates_To-Work/sass-react/DynamicHeroUI/src/components/mainContent.tsx
+++ Templates_To-Work/sass-react/DynamicHeroUI/src/components/mainContent.tsx
@@ -1,14 +1,56 @@
 import HeroImg from "../assets/HeroImg.png";
-import Rightpart from "../assets/rightpart.png";
+import Rightpart from "../assets/rightpartt.png";
+import RightImg from "../assets/rightIMG.png";
 
 export default function MainContent() {
   return (
     <div className="mainContentPart">
       <div className="section_hero-img-left">
-        <img src={HeroImg} alt="" />
+        <img src={HeroImg} className="img1" alt="" />
+        <img src={HeroImg} className="img2" alt="" />
       </div>
       <div className="section_hero-content-right">
-           <img src={Rightpart} alt="" />
+        {/* search */}
+        <div className="topbox">
+          <div className="backgIcon">
+            <i
+              className="fa fa-instagram"
+              id="iconinstagram"
+              aria-hidden="true"
+            ></i>
+          </div>
+
+          <div className="search_bar">
+            <button className="searchInput">
+              <i className="fa fa-search" aria-hidden="true"></i>
+              <p>SEARCH</p>
+            </button>
+            <button className="searchFilter">
+              <div className="item1"></div>
+              <div className="item2"></div>
+              <div className="item3"></div>
+            </button>
+          </div>
+          <div className="backgCercle">
+            <div className="cercle"></div>
+          </div>
+        </div>
+        <img src={Rightpart} alt="" />
+
+        {/* down box */}
+        <div className="borderradius"></div>
+        <div className="downbox">
+          <div className="titlepart">
+            <p>40% OFF</p>
+            <span>ON RACKET</span>
+            <button>SHOP NOW</button>
+          </div>
+          <div className="boximg">
+            <img src={RightImg} className="boximg1" alt="" />
+            <img src={RightImg} className="boximg2" alt="" />
+
+          </div>
+        </div>
       </div>
     </div>
   );
